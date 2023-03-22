d0bc15023d285c6f59b81d9e9de110e27e6d07f2
Eduardo Martinbiancho <martinbianchoeduardo@gmail.com>
2023-03-22 11:12:07 -0300

    add jwt

--- notes/project/4-natours/starter/controllers/authController.js
+++ notes/project/4-natours/starter/controllers/authController.js
@@ -1,19 +1,30 @@
 const User = require('./../models/userModel');
+const jwt = require('jsonwebtoken');
 
 exports.signup = async (req, res, next) => {
   try {
-    const newUser = await User.create(req.body);
+    const newUser = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password
+    });
+
+    //.sign(payload , secret , options)
+    const token = jwt.sign({ id: newUser._id }, 'supersecretkey', {
+      expiresIn: '90d'
+    });
 
     res.status(201).json({
       status: 'success',
+      token: token,
       data: {
-        newUser
+        user: newUser
       }
     });
   } catch (err) {
     res.status(400).json({
       status: 'fail',
-      message: err
+      message: err.message
     });
   }
 };
