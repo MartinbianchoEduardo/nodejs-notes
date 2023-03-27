6cc75ea387da2a9d3276eefe7f9967c40125c9e9
Eduardo Martinbiancho <martinbianchoeduardo@gmail.com>
2023-03-27 08:55:19 -0300

    add review model

---
+++ notes/project/4-natours/starter/models/reviewModel.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema({
+  review: { type: String, required: [true, 'a review cannot be empty'] },
+  rating: {
+    type: Number,
+    require: [true, 'a rating is required'],
+    min: 1,
+    max: 5
+    //min and max only work for numbers in mongoose
+  },
+  createdAt: { type: Date, default: Date.now },
+  tour: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Tour',
+    required: [true, 'the review must belong to a tour']
+  },
+  author: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: [true, 'the review must belong to an author']
+  }
+});
+
+const Review = new mongoose.model('Review', userSchema);
+
+module.exports = Review;
